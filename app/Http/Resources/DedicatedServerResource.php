fe6b2929fca195f5880ebeeff2088124d4aca9c3
Onur Uslu <onrslu@gmail.com>
2022-09-13 00:33:39 +0300

    feature: reproducement of bug environment.

---
+++ app/Http/Resources/DedicatedServerResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/** @mixin \App\Models\DedicatedServer */
+class DedicatedServerResource extends JsonResource
+{
+    /**
+     * @param Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'product_id' => $this->product_id,
+            'type' => $this->type,
+            'processor_line_1' => $this->processor_line_1,
+            'processor_line_2' => $this->processor_line_2,
+            'memory' => $this->memory,
+            'storage_line_1' => $this->storage_line_1,
+            'storage_line_2' => $this->storage_line_2,
+            'data' => $this->data,
+            'benchmark' => $this->benchmark,
+            'product' => ProductResourceCollection::make($this->whenLoaded('product')),
+        ];
+    }
+}
---
+++ app/Http/Resources/ProductResourceCollection.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/** @mixin \App\Models\Product */
+class ProductResourceCollection extends JsonResource
+{
+    /**
+     * @param Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'tagline' => $this->tagline,
+            'slug' => $this->slug,
+            'dedicatedServers' => $this->whenLoaded('dedicated_servers'),
+        ];
+    }
+}
